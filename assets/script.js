6ce724b187036c1813283eedaddf9dbe10fb2cc9
Clowenthal <chrislowenthal1@gmail.com>
2023-08-25 13:52:42 -0700

    JavaScript Prework study guide update

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+
+/*
+
+if (topic === 'HTML') {
+    
+    console.log("Let's study HTML!");
+
+    } else if (topic === 'CSS') {
+        
+        console.log("Let's study CSS!");
+
+    } else if (topic === 'GIT') {
+
+        console.log("Let's study GIT!");
+    
+    } else if (topic === 'JAVASCRIPT') {
+        
+        console.log("Let's study Javascript!");
+    
+    } else {
+
+        console.log('Please try again!')
+    
+    }
+
+    */
